02cb55d6cd69054aebff1feaaba4d35be06b87c9
yjzhang <yjzhang@cs.washington.edu>
2022-09-16 16:19:56 -0700

    add minimal model

---
+++ mm0.py
@@ -0,0 +1,42 @@
+# implementation of bergman's minimal model in tellurium
+# https://journals.physiology.org/doi/pdf/10.1152/ajpendo.1979.236.6.e667
+# https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7917251/
+
+# the first one is the model we should use.
+
+import tellurium as te
+
+# I, X, G represent plasma insulin, remote insulin, and plasma glucose.
+# Ib is the initial concentration of plasma insulin
+# u1(t) is exogenous insulin
+# u2(t) is intravenous glucose
+# each time point is 1 minute
+
+mm0 = te.loada('''
+        model minimal
+          G -> ; p1*G + X*G
+          -> G; p1*Gb + u2/Vol
+
+          I -> ; n*I
+          -> I ; p4*u1
+
+          X -> ; p2*X
+          -> X ; p3*(I - Ib)
+
+          p1 = 0.035
+          p2 = 0.05
+          p3 = 0.000028
+          p4 = 0.098
+          n = 0.142
+          Vol = 117.0
+          Gb = 80
+
+          Ib = p4/n
+          X = 0
+          G = Gb
+          I = Ib
+
+          u1 = 0
+          u2 = 0
+        end
+        ''')
